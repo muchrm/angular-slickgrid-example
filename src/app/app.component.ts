96f52a05dfc845546dc73e23169086575595edf8
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2020-04-20 01:41:22 +0700

    add new data when expand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,89 +32,25 @@ export class AppComponent {
   ngOnInit(): void {
     this.columnDefinitions = [
       {
-        id: 'sel', name: '#', field: 'num', width: 40,
-        excludeFromExport: true,
-        maxWidth: 70,
-        resizable: true,
-        filterable: true,
-        selectable: false,
-        focusable: false
-      },
-      {
-        id: 'title', name: 'Title', field: 'title',
-        width: 50,
-        minWidth: 50,
-        cssClass: 'cell-title',
-        filterable: true,
-        sortable: true
-      },
-      {
-        id: 'duration', name: 'Duration', field: 'duration',
+        id: 'id', name: 'Id', field: 'id',
         minWidth: 50, width: 60,
-        filterable: true,
-        filter: { model: Filters.slider, operator: '>=' },
-        sortable: true,
+      },
+      {
+        id: 'group', name: 'group', field: 'group',
+        minWidth: 50, width: 60,
         type: FieldType.number,
-        groupTotalsFormatter: GroupTotalFormatters.sumTotals,
-        params: { groupFormatterPrefix: 'Totalx: ' }
-      },
-      {
-        id: '%', name: '% Complete', field: 'percentComplete',
-        minWidth: 70, width: 90,
-        formatter: Formatters.percentCompleteBar,
-        filterable: true,
-        filter: { model: Filters.compoundSlider },
-        sortable: true,
-        type: FieldType.number,
-        // groupTotalsFormatter: GroupTotalFormatters.avgTotalsPercentage,
-        // params: { groupFormatterPrefix: '<i>Avg</i>: ' }
-      },
-      {
-        id: 'start', name: 'Start', field: 'start',
-        minWidth: 60,
-        filterable: true,
-        filter: { model: Filters.compoundDate },
-        sortable: true,
-        type: FieldType.dateIso,
-        formatter: Formatters.dateIso,
-        exportWithFormatter: true
-      },
-      {
-        id: 'finish', name: 'Finish', field: 'finish',
-        minWidth: 60,
-        filterable: true,
-        filter: { model: Filters.compoundDate },
-        sortable: true,
-        type: FieldType.dateIso,
-        formatter: Formatters.dateIso,
-        exportWithFormatter: true
-      },
-      {
-        id: 'cost', name: 'Cost', field: 'cost',
-        minWidth: 70,
-        width: 100,
-        filterable: true,
-        filter: { model: Filters.compoundInputNumber },
-        type: FieldType.number,
-        sortable: true,
-        exportWithFormatter: true,
-        formatter: Formatters.dollar,
-        // groupTotalsFormatter: GroupTotalFormatters.sumTotalsDollar,
-        // params: { groupFormatterPrefix: '<b>Total</b>: ' /*, groupFormatterSuffix: ' USD'*/ }
-      },
-      {
-        id: 'effort-driven', name: 'Effort Driven',
-        minWidth: 20, width: 80, maxWidth: 80,
-        cssClass: 'cell-effort-driven',
-        field: 'effortDriven',
-        formatter: Formatters.checkmark,
-        sortable: true,
-        filterable: true,
-        filter: {
-          collection: [{ value: '', label: '' }, { value: true, label: 'True' }, { value: false, label: 'False' }],
-          model: Filters.singleSelect,
+        groupTotalsFormatter: (totals, columnDef) => {
+          return 'totalX: ddkdkdkd'
         }
-      }
+      },
+      {
+        id: 'gender', name: 'gender', field: 'gender',
+        minWidth: 50, width: 60,
+        type: FieldType.number,
+        groupTotalsFormatter: (totals, columnDef) => {
+          return 'totalY: ddkdkdkd'
+        }
+      },
     ];
 
     this.gridOptions = {
@@ -122,18 +58,7 @@ export class AppComponent {
         containerId: 'demo-container',
         sidePadding: 10
       },
-      enableExcelExport: true,
-      enableFiltering: true,
       enableGrouping: true,
-      exportOptions: {
-        sanitizeDataExport: true
-      },
-      excelExportOptions: {
-        sanitizeDataExport: true
-      },
-      gridMenu: {
-        hideExportTextDelimitedCommand: false
-      }
     };
 
 
@@ -146,10 +71,10 @@ export class AppComponent {
     this.dataviewObj = angularGrid.dataView;
     this.dataviewObj.setGrouping([
       {
-        getter: 'duration',
+        getter: 'group',
         formatter: (g) => {
           // console.log(g);
-          return `Duration: ${g.value}  <span style="color:green">(${g.count} items)</span>`;
+          return `group: ${g.value}  <span style="color:green">(${g.count} items)</span>`;
         } ,
         aggregators: [
           new SelectGroupAggregator(0),
@@ -157,41 +82,65 @@ export class AppComponent {
         collapsed: true,
       },
       {
-        getter: 'effortDriven',
-        formatter: (g) => `Effort-Driven: ${(g.value ? 'True' : 'False')} <span style="color:green">(${g.count} items) ${g.totals.group.title}</span> `,
+        getter: 'gender',
+        formatter: (g) => `gender: ${(g.value ? 'True' : 'False')} <span style="color:green">(${g.count} items) ${g.totals.group ? g.totals.group.title:''}</span> `,
         aggregators: [
           new SelectGroupAggregator(1),
         ],
         collapsed: true,
       }
     ] as Grouping[]);
-    this.angularGrid.dataView.onGroupExpanded.subscribe(console.log);
+    this.angularGrid.dataView.onGroupExpanded.subscribe((e,args)=> {
+      this.angularGrid.dataView
+      console.log(args)
+      this.dataset.push(
+        {
+          "name": "Ethel Price",
+          "gender": 2,
+          "company": "Enersol",
+          "id": this.dataset.length,
+          "group": 1
+        },
+      )
+    });
     this.angularGrid.dataView.onGroupCollapsed.subscribe(console.log);
   }
 
   loadData(rowCount: number) {
     // mock a dataset
-    this.dataset = [];
-    for (let i = 0; i < rowCount; i++) {
-      const randomYear = 2000 + Math.floor(Math.random() * 10);
-      const randomMonth = Math.floor(Math.random() * 11);
-      const randomDay = Math.floor((Math.random() * 29));
-      const randomPercent = Math.round(Math.random() * 100);
+    this.dataset = [
+      {
+        "name": "Ethel Price",
+        "gender": 2,
+        "company": "Enersol",
+        "id": 1,
+        "group": 1
+      },
+      {
+        "name": "Claudine Neal",
+        "gender": 1,
+        "company": "Sealoud",
+        "id": 2,
+        "group": 2
+      },
+      {
+        "name": "Beryl Rice",
+        "gender": 2,
+        "company": "Velity",
+        "id": 3,
+        "group": 3
+      },
+      {
+        "name": "Wilder Gonzales",
+        "gender": 1,
+        "company": "Geekko",
+        "id": 4,
+        "group": 4
+      },
+    ];
+  }
 
-      this.dataset[i] = {
-        id: 'id_' + i,
-        num: i,
-        title: 'Task ' + i,
-        duration: Math.round(Math.random() * 100) + '',
-        percentComplete: randomPercent,
-        percentCompleteNumber: randomPercent,
-        start: new Date(randomYear, randomMonth, randomDay),
-        finish: new Date(randomYear, (randomMonth + 1), randomDay),
-        cost: (i % 33 === 0) ? null : Math.round(Math.random() * 10000) / 100,
-        effortDriven: (i % 5 === 0),
-        isGroupKey:true,
-        level:1,
-      };
-    }
+  setFormat(c) {
+    return c;
   }
 }
